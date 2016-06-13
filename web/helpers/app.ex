2865d1901352a85b2afd47c07affbf7e221a1169
Jeremie ges <ges.jeremie@gmail.com>
2016-06-13 05:59:43 +0300

    improve helper app

--- web/helpers/app.ex
+++ web/helpers/app.ex
@@ -1,7 +1,19 @@
 defmodule Babysitting.Helpers.App do
   
-  def host(conn) do
-    conn.private |> Map.get(:website)
+  def current_tenant(conn) do
+    conn.private |> Map.get(:tenant)
+  end
+
+  def current_tenant_domain(conn) do
+    current_tenant(conn)[:domain]
+  end
+
+  def current_tenant_slug(conn) do
+    current_tenant(conn)[:slug]
+  end
+
+  def current_tenant_name(conn) do
+    current_tenant(conn)[:name]
   end
 
 end
