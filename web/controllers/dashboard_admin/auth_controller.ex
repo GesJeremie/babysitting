763fbc1d1f028563162c29c28a117b1957823502
Jeremie ges <ges.jeremie@gmail.com>
2016-07-10 05:32:59 +0300

    improvement

--- web/controllers/dashboard_admin/auth_controller.ex
+++ web/controllers/dashboard_admin/auth_controller.ex
@@ -8,30 +8,35 @@ defmodule Babysitting.DashboardAdmin.AuthController do
   end
 
   def login(conn, %{"login" => %{"password" => password}}) do
-    
+
     conn = conn |> fetch_session
 
-    count_attempt_login = conn |> get_session(:count_attempt_login) || 1
-
-    if count_attempt_login <= 10 do
-
-      if @password_login == password do
-        conn
-          |> put_session(:is_admin, true)
-          |> delete_session(:count_attempt_login)
-          |> redirect(to: "/dashboard")
-      else
-        conn
-          |> put_session(:count_attempt_login, count_attempt_login + 1)
-          |> put_flash(:error, "Impossible to connect with this password")
-          |> redirect(to: "/dashboard/auth")
-      end
-
-    else 
+    if count_attempt_login(conn) <= 10 do
       conn
+        |> attempt_login(password)
+    else 
+      conn 
         |> redirect(to: "/")
     end
 
+  end
+
+  defp count_attempt_login(conn) do
+    (conn |> get_session(:count_attempt_login)) || 1
+  end
+
+  defp attempt_login(conn, password) when password == @password_login do
+      conn
+        |> put_session(:is_admin, true)
+        |> delete_session(:count_attempt_login)
+        |> redirect(to: "/dashboard")
+  end
+
+  defp attempt_login(conn, _) do
+    conn
+      |> put_session(:count_attempt_login, count_attempt_login(conn) + 1)
+      |> put_flash(:error, "Impossible to connect with this password")
+      |> redirect(to: "/dashboard/auth")
   end
 
   def logout(conn, _params) do
