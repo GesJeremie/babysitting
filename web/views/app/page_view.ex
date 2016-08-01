b94de11826eab1ddebad3cd72a98fde6575c628e
Jeremie ges <ges.jeremie@gmail.com>
2016-08-01 03:45:25 +0300

    home index fix bug

--- web/views/app/page_view.ex
+++ web/views/app/page_view.ex
@@ -1,9 +1,23 @@
 defmodule Babysitting.App.PageView do
   use Babysitting.Web, :view
+  use Phoenix.HTML
+
+  alias Babysitting.Helpers.{Date, Format}
 
   def town(town) do
     town |> String.capitalize
   end
+
+  def age(%{:birthday => birthday}) do
+    Date.age(birthday)
+  end
+
+  def short_description(%{:description => description}) do
+    description
+      |> Format.word_limit
+      |> text_to_html
+  end
+
 
 
 end
