1868b06b1e67cccaee49bcb69c7aa5d4ea8d0a65
Jeremie ges <ges.jeremie@gmail.com>
2016-10-02 19:05:37 +0300

    Add tests App helper

--- test/helpers/app_helper_test.exs
+++ test/helpers/app_helper_test.exs
@@ -1,22 +1,57 @@
 defmodule Babysitting.AppHelperTest do
-
   use Babysitting.ConnCase
   import Babysitting.ConnCase
-  alias Babysitting.Helpers.App
   import Babysitting.Factory
+  alias Babysitting.Helpers.App
 
-  test "current_tenant/1 return a map of the current tenant" do
+  describe "current_tenant/1" do
 
-    conn =
-      build_conn()
-      |> with_host("www.babysittingbordeaux.dev")
-      |> with_tenant
+    setup do 
+      conn =
+        build_conn()
+        |> with_host("www.babysittingbordeaux.dev")
+        |> with_tenant
 
-    assert conn
-      |> App.current_tenant
-      |> is_map
+      [conn: conn]
+    end
+
+    test "return a map", context do
+      assert App.current_tenant(context.conn) |> is_map
+    end
+
+    test "return the properties", context do
+      tenant = App.current_tenant(context.conn)
+
+      for property <- [:domain, :id, :name, :slug, :facebook, :locale] do
+        assert tenant |> Map.has_key?(property)
+      end
+    end
 
   end
+
+  describe "current_user/1" do
+
+    setup do
+      classified = insert(:classified)
+
+      conn =
+        build_conn()
+        |> with_session
+        |> put_session(:current_user, classified.id)
+
+      [conn: conn]
+    end
+
+    test "return a map", context do
+      assert App.current_user(context.conn) |> is_map
+    end
+
+    test "return id", context do
+      assert App.current_user(context.conn) |> Map.has_key?(:id)
+    end
+
+  end
+
 
   test "tenants/1 return a list of the tenants" do
     conn =
