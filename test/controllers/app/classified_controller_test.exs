a729871998dee3f29f00ce32b3410248b7bc231d
Jeremie ges <ges.jeremie@gmail.com>
2016-10-10 20:06:17 +0300

    Classified controller test app in progress

--- test/controllers/app/classified_controller_test.exs
+++ test/controllers/app/classified_controller_test.exs
@@ -1,73 +1,45 @@
-"""
 defmodule Babysitting.ClassifiedControllerTest do
   use Babysitting.ConnCase
   alias Babysitting.Classified
-  @valid_attrs %{}
-  @invalid_attrs %{}
+  import Babysitting.Factory
 
-  test "awesome", %{conn: conn} do
-    IO.inspect conn
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Bordeaux"
-  end
-
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, offer_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing offers"
-  end
-
-  test "renders form for new resources", %{conn: conn} do
-    conn = get conn, offer_path(conn, :new)
-    assert html_response(conn, 200) =~ "New offer"
-  end
-
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, offer_path(conn, :create), offer: @valid_attrs
-    assert redirected_to(conn) == offer_path(conn, :index)
-    assert Repo.get_by(Offer, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, offer_path(conn, :create), offer: @invalid_attrs
-    assert html_response(conn, 200) =~ "New offer"
-  end
-
-  test "shows chosen resource", %{conn: conn} do
-    offer = Repo.insert! %Offer{}
-    conn = get conn, offer_path(conn, :show, offer)
-    assert html_response(conn, 200) =~ "Show offer"
-  end
-
-  test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, offer_path(conn, :show, -1)
+  describe "new/2" do
+    test "display form", %{conn: conn} do
+      conn = get conn, app_classified_path(conn, :new)
+      assert html_response(conn, 200)
     end
   end
 
-  test "renders form for editing chosen resource", %{conn: conn} do
-    offer = Repo.insert! %Offer{}
-    conn = get conn, offer_path(conn, :edit, offer)
-    assert html_response(conn, 200) =~ "Edit offer"
+  describe "create/2" do
+    test "does not create resource when form invalid", %{conn: conn} do
+      conn = post conn, app_classified_path(conn, :create), classified: %{}
+
+      assert html_response(conn, 200) =~ "<form"
+      assert Repo.all(Classified) |> length == 0
+    end
+
+    test "create resource when form valid", %{conn: conn} do
+      params = %{
+        firstname: FakerElixir.Name.first_name,
+        lastname: FakerElixir.Name.last_name,
+        birthday: "03/10/1991",
+        description: FakerElixir.Lorem.characters(300),
+        avatar: %Plug.Upload{path: "test/fixtures/avatar.jpeg", filename: "avatar.jpeg"},
+        email: FakerElixir.Internet.email(:popular),
+        phone: FakerElixir.Helper.numerify("##########"),
+        password: "awesomepassword",
+        password_confirmation: "awesomepassword"
+      }
+
+      conn = post conn, app_classified_path(conn, :create), classified: params
+
+      IO.inspect params
+      IO.inspect conn
+
+    end
+
+    
+
   end
 
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    offer = Repo.insert! %Offer{}
-    conn = put conn, offer_path(conn, :update, offer), offer: @valid_attrs
-    assert redirected_to(conn) == offer_path(conn, :show, offer)
-    assert Repo.get_by(Offer, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    offer = Repo.insert! %Offer{}
-    conn = put conn, offer_path(conn, :update, offer), offer: @invalid_attrs
-    assert html_response(conn, 200) =~ "Edit offer"
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    offer = Repo.insert! %Offer{}
-    conn = delete conn, offer_path(conn, :delete, offer)
-    assert redirected_to(conn) == offer_path(conn, :index)
-    refute Repo.get(Offer, offer.id)
-  end
 end
-"""
