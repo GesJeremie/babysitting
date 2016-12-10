5577898c640c2529574efb0552d7fd4aa2992bf4
Jeremie ges <ges.jeremie@gmail.com>
2016-12-10 06:29:09 +0200

    Fix problem footer

--- web/views/app/partials_view.ex
+++ web/views/app/partials_view.ex
@@ -10,8 +10,21 @@ defmodule Babysitting.App.PartialsView do
     App.current_tenant(conn).facebook
   end
 
+  ##
+  # Tenants is used to display the link for the other 
+  # websites. We want to fetch only the websites in 
+  # production.
+  # 
+  # Note: It could be improved with a migration and setup
+  # a field "environment" in the table tenant. Let's say
+  # it's okay for the time being.
+  ###
   def tenants(conn) do
-    App.tenants(conn)
+    conn
+    |> App.tenants
+    |> Enum.filter(fn(tenant) ->
+      (tenant.domain |> String.slice(-3, 3)) !== "dev"
+    end)
   end
 
 end
