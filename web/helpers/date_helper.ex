d9e29bce9a88c78056aca9129a40aa348dfc6c29
Ges Jérémie <ges.jeremie@gmail.com>
2016-12-27 03:35:58 +0200

    Integrate intercom

--- web/helpers/date_helper.ex
+++ web/helpers/date_helper.ex
@@ -17,5 +17,15 @@ defmodule Babysitting.Helpers.DateHelper do
     Timex.diff(now, birthday, :years)
   end
 
+  @doc """
+  Convert ecto datetime to unix timestamp
+  Note: https://elixirforum.com/t/how-to-convert-ecto-datetime-to-unix-timestamps/1165
+  """
+  def ecto_datetime_to_timestamp(datetime) do
+    datetime
+    |> Ecto.DateTime.to_erl
+    |> :calendar.datetime_to_gregorian_seconds
+    |> Kernel.-(62167219200)
+  end
 
 end
--- web/views/partials_view.ex
+++ web/views/partials_view.ex
@@ -1,6 +1,7 @@
 defmodule Babysitting.PartialsView do
   use Babysitting.Web, :view
-  alias Babysitting.Helpers.{AppHelper}
+  alias Babysitting.Helpers.{AppHelper, FormatHelper, DateHelper}
+  import Plug.Conn
 
   def hotjar_id(conn) do
     AppHelper.current_tenant(conn).hotjar
@@ -9,4 +10,26 @@ defmodule Babysitting.PartialsView do
   def google_analytics_id(conn) do
     AppHelper.current_tenant(conn).google_analytics
   end
+
+  def user_logged?(conn) do
+
+    IO.inspect conn
+    |> fetch_session
+    |> get_session(:current_user)
+    |> do_user_logged(conn)
+  end
+
+  defp do_user_logged(id, conn) when is_integer(id), do: true
+  defp do_user_logged(_, conn), do: false
+
+  def user(conn) do
+    classified = AppHelper.current_user(conn)
+
+    %{
+      name: FormatHelper.fullname(classified.firstname, classified.lastname),
+      email: classified.email,
+      created_at: DateHelper.ecto_datetime_to_timestamp(classified.inserted_at)
+    }
+  end
+
 end
