51cb4d329cd99a1da0a6ca3c6359c9f9962cd3e6
Ges Jérémie <ges.jeremie@gmail.com>
2017-03-02 00:48:59 +0100

    New fix json ld micro data

--- web/helpers/micro_data_helper.ex
+++ web/helpers/micro_data_helper.ex
@@ -5,26 +5,25 @@ defmodule Babysitting.Helpers.MicroDataHelper do
   Render micro data json ld for a classified
   """
   def classified(conn, classified) do
-    Phoenix.HTML.raw ~s(
-      <script type="application/ld+json">
-        {
-          "@context": "http://schema.org/",
-          "@type": "Service",
-          "serviceType": "Baby Sitting",
-          "provider": {
-            "@type": "Person",
-            "givenName": "#{Phoenix.HTML.escape_javascript(classified.firstname)}",
-            "familyName": "#{Phoenix.HTML.escape_javascript(classified.lastname)}",
-            "birthDate": "#{Phoenix.HTML.escape_javascript(classified.birthday)}",
-            "email": "#{Phoenix.HTML.escape_javascript(classified.email)}",
-            "telephone": "#{Phoenix.HTML.escape_javascript(classified.phone)}"
-          },
-          "areaServed": {
-            "@type": "City",
-            "name": "#{AppHelper.current_tenant(conn).name}"
-          },
-          "description": "#{Phoenix.HTML.escape_javascript(classified.description)}"
-        }
-      </script>)
+    json = Poison.encode! %{
+      "@context" => "http://schema.org/",
+      "@type" => "Service",
+      "serviceType" => "Baby Sitting",
+      "provider" => %{
+        "@type" => "Person",
+        "givenName" => classified.firstname,
+        "familyName" => classified.lastname,
+        "birthDate" => classified.birthday,
+        "email" => classified.email,
+        "telephone" => classified.phone
+      },
+      "areaServed" => %{
+        "@type" => "City",
+        "name" => AppHelper.current_tenant(conn).name
+      },
+      "description" => classified.description
+    }
+
+    Phoenix.HTML.raw ~s(<script type="application/ld+json">#{json}</script>)
   end
 end
